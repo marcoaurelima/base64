d82eaf076c208ac7265cb790cf63b6d22d7b68f8
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-06-02 02:28:12 -0300

    Remoção de comentários desnecessário.

--- main.cpp
+++ main.cpp
@@ -25,16 +25,9 @@ string base64encoder(vector<long int> input)
         UCHAR byte_5 = (input[(3*i)+2] & 0x3F) >> 0;  // 0x3F = 00111111
 
         result << b64[byte_0] << b64[byte_1 + byte_2] << b64[byte_3 + byte_4] << b64[byte_5];
-
-        cout << "* " << (char) input[(3*i)+2] << "\n";
     }
 
-    puts("");
-
-    if(input.size()%3 == 0)  {
-        return result.str();
-    }
-
+    if(input.size()%3 == 0) { return result.str(); }
 
     if(input.size()%3 == 1)
     {
@@ -43,10 +36,7 @@ string base64encoder(vector<long int> input)
         UCHAR byte_2 = '=';
         UCHAR byte_3 = '=';
 
-        result << b64[byte_0] << b64[byte_1] << byte_2 << byte_3;//<< b64[byte_1 + byte_2] << b64[byte_3] << byte_4 << byte_5;
-
-        cout << "sobrou 1:\n";
-        return result.str();
+        result << b64[byte_0] << b64[byte_1] << byte_2 << byte_3;
     }
 
     if(input.size()%3 == 2)
@@ -56,39 +46,24 @@ string base64encoder(vector<long int> input)
         UCHAR byte_1 = (input[input.size()-1] & 0x03) << 4;  // 0x03 = 00000011
         UCHAR byte_2 = (input[input.size()-1] & 0xF0) >> 4;  // 0xF0 = 11110000
         UCHAR byte_3 = (input[input.size()-1] & 0x0F) << 2;  // 0x0F = 00001111
-        //UCHAR byte_4 = (input[input.size()-1] & 0xC0) >> 6;  // 0xC0 = 11000000
-        //UCHAR byte_5 = (input[input.size()-1] & 0x3F) >> 0;  // 0x3F = 00111111
         UCHAR byte_4 = '=';
 
-        /*
-        UCHAR byte_0 = (input[input.size()-1] & 0xFC) >> 2;  // 0xFC = 11111100
-        UCHAR byte_1 = (input[input.size()-1] & 0x03) << 4;  // 0x03 = 00000011
-        UCHAR byte_2 = (input[input.size()-2] & 0x0F) >> 2;  // 0xF0 = 11110000
-        UCHAR byte_3 = (input[input.size()-3] & 0xC0) >> 6;  // 0xC0 = 11000000
-        UCHAR byte_4 = '=';*/
-
-
-        result << b64[byte_0] << b64[byte_1 + byte_2] << b64[byte_3] << byte_4;// b64[byte_5];
-
-        cout << "sobrou 2:\n";
-        return result.str();
+        result << b64[byte_0] << b64[byte_1 + byte_2] << b64[byte_3] << byte_4;
     }
 
-
-    return "";
-
+    return result.str();
 }
 
 
 int main()
 {
-    string input = "marco aurelio lima de sousas";
+    string input = "Michel Machado Monteiro";
     vector<long int> nome;
     for(auto i : input) { nome.push_back(i); }
 
     auto res = base64encoder(nome);
 
-    cout << "bWFyY28=\n" << res << "\n";
+    cout << res << "\n";
 
     return 0;
 }
