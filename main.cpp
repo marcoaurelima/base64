96598c7f74d624c24f726d891e1e94600786cf48
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-06-03 22:06:49 -0300

    Apagando comentários dsnessesários.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@ using UCHAR = unsigned char;
 
 using namespace std;
 
-string b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-             "abcdefghijklmnopqrstuvwxyz"
-             "0123456789+/";
+
 
 string base64encoder(vector<long int> input)
 {
+    string b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     stringstream result;
 
     for(size_t i=0;i<input.size()/3;++i)
@@ -31,25 +30,20 @@ string base64encoder(vector<long int> input)
 
     if(input.size()%3 == 1)
     {
-        puts("sobra 1");
-        UCHAR byte_0 = (input[input.size()-1] & 0xFC) >> 2;
-        UCHAR byte_1 = (input[input.size()-1] & 0x03) << 4;
+        UCHAR byte_0 = (input[input.size()-1] & 0xFC) >> 2; // 0xFC = 11111100
+        UCHAR byte_1 = (input[input.size()-1] & 0x03) << 4; // 0x03 = 00000011
         UCHAR byte_2 = '=';
         UCHAR byte_3 = '=';
-
         result << b64[byte_0] << b64[byte_1] << byte_2 << byte_3;
     }
 
     if(input.size()%3 == 2)
     {
-        puts("sobra 2");
-
         UCHAR byte_0 = (input[input.size()-2] & 0xFC) >> 2;  // 0xFC = 11111100
         UCHAR byte_1 = (input[input.size()-2] & 0x03) << 4;  // 0x03 = 00000011
         UCHAR byte_2 = (input[input.size()-1] & 0xF0) >> 4;  // 0xF0 = 11110000
         UCHAR byte_3 = (input[input.size()-1] & 0x0F) << 2;  // 0x0F = 00001111
         UCHAR byte_4 = '=';
-
         result << b64[byte_0] << b64[byte_1 + byte_2] << b64[byte_3] << byte_4;
     }
 
@@ -59,7 +53,7 @@ string base64encoder(vector<long int> input)
 
 int main()
 {
-    string input = "Michel Machado Monteiro @!";
+    string input = "Do you have to deal with Base64 format? Then this site is perfect for you! Use our super handy online tool to encode or decode your data";
     vector<long int> nome;
     for(auto i : input) { nome.push_back(i); }
 
